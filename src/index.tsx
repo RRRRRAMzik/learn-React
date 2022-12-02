b139e4f687ece89f7857e7d1416ecb0cf9d696f7
RRRRRAMzik <75689935+RRRRRAMzik@users.noreply.github.com>
2022-12-03 03:56:51 +0700

    Create React app

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const rootHtmlElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootHtmlElement);
+root.render(
+    <React.StrictMode>
+    </React.StrictMode>
+);
