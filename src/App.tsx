49cd18782bc4ea09675ec0da56c65ed189ab920d
RRRRRAMzik <75689935+RRRRRAMzik@users.noreply.github.com>
2022-12-12 01:21:14 +0700

    Added useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import randomInt from './utils/randomInt';
 
 
 export default function App() {
-    const a = randomInt(1, 10);
-    const b = randomInt(1, 10);
-    const c = a * b;
+    const [A, setA] = useState(randomInt(1, 10));
+    const [B, setB] = useState(randomInt(1, 10));
+    const [C, setC] = useState(A * B);
+    function generate() {
+        setA(randomInt(1, 10));
+        setB(randomInt(1, 10));
+        setC(A * B);
+    }
     return (
         <>
-            {a} * {b} = {c}
+            <p>
+                {A} * {B} = {C}
+            </p>
+            <button onClick={generate}>
+                Новый пример
+            </button>
         </>
     );
 }
