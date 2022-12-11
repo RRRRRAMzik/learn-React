19003d4cc05430a4dba8105bea95eec6b060fddb
RRRRRAMzik <75689935+RRRRRAMzik@users.noreply.github.com>
2022-12-12 02:01:17 +0700

    Added generate of answers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import randomInt from './utils/randomInt';
 
 
+const NUMBER_ANSWERS = 4;
+
+
 export default function App() {
-    const [A, setA] = useState(randomInt(1, 10));
-    const [B, setB] = useState(randomInt(1, 10));
-    const [C, setC] = useState(A * B);
+    const [A, setA] = useState(0);
+    const [B, setB] = useState(0);
+    const [answers, setAnswers] = useState(new Set<number>());
+
     function generate() {
-        setA(randomInt(1, 10));
-        setB(randomInt(1, 10));
-        setC(A * B);
+        const a = randomInt(1, 10);
+        const b = randomInt(1, 10);
+        const tempAnswers = new Set<number>([a * b]);
+        while (tempAnswers.size < NUMBER_ANSWERS) {
+            tempAnswers.add(randomInt(1, 81));
+        }
+        setA(a);
+        setB(b);
+        setAnswers(tempAnswers);
     }
+    useEffect(generate,[]);
+    function selectAnswer(answer: number) {
+
+    }
+
     return (
         <>
             <p>
-                {A} * {B} = {C}
+                {A} * {B} =
+                {Array.from(answers).map((answer) => <button onClick={() => selectAnswer(answer)}>{answer}</button>)}
             </p>
             <button onClick={generate}>
                 Новый пример
